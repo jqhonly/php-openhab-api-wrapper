2cb3db624306ed36aae8894642a6517c3d61f020
ms <schmiddim@gmx.at>
2016-01-29 23:39:37 +0100

    refactoring

---
+++ src/Openhab/Collections/ItemCollection.php
@@ -0,0 +1,68 @@
+<?php
+
+
+namespace Openhab\Collections;
+
+use Openhab\Item\FilterItem;
+use Openhab\Item\Item;
+
+class ItemCollection
+{
+	protected $items = array();
+
+	public function findByFilter(FilterItem $filter)
+	{
+		$resultSet = array();
+		foreach ($this->getItems() as $item) {
+			/**@var Item $item * */
+			if ($filter->getName() !== null) {
+				if ($item->getName() === $filter->getName()) {
+					$resultSet[] = $item;
+				}
+			}
+			if ($filter->getType() !== null) {
+				if ($item->getType() === $filter->getType()) {
+					$resultSet[] = $item;
+				}
+			}
+			if ($filter->getState() !== null) {
+				if ($item->getState() === $filter->getState()) {
+					$resultSet[] = $item;
+				}
+			}
+			if ($filter->getLink() !== null) {
+				if ($item->getLink() === $filter->getLink()) {
+					$resultSet[] = $item;
+				}
+			}
+		}
+		return $resultSet;
+	}
+
+	/**
+	 * @param Item $item
+	 */
+	public function add(Item $item)
+	{
+
+		$this->items[] = $item;
+	}
+
+	/**
+	 * @return array
+	 */
+	public function getItems()
+	{
+		return $this->items;
+	}
+
+	/**
+	 * @param array $items
+	 */
+	public function setItems($items)
+	{
+		$this->items = $items;
+	}
+
+
+}
