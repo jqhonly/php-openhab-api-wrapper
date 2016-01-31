2c0ef8f0eb72652bbfde278df873aafa8fb236e9
ms <schmiddim@gmx.at>
2016-01-31 12:21:55 +0100

    deployment

--- deploy.php
+++ deploy.php
@@ -1,34 +1,28 @@
 <?php
 require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'vendor/autoload.php';
+require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'config.php';
 use Secondtruth\Compiler\Compiler;
 
 $compiler = new Compiler('./');
 
-$compiler->addIndexFile('cli.php');
-$compiler->addFile('vendor/autoload.php');
-$compiler->addFile('config.php');
-$compiler->addDirectory('vendor/composer', '!*.php');
-$compiler->addDirectory('vendor/zendframework', '!*.php');
-$compiler->addDirectory('vendor/league', '!*.php');
-$compiler->addDirectory('src', '!*.php');
-$compiler->compile('.' . DIRECTORY_SEPARATOR . 'deploy/cli.phar');
+$pharFiles = array(
+	'cli.php' => '.' . DIRECTORY_SEPARATOR . 'deploy/openhab-cli.phar',
+	'openhab-command.php' => '.' . DIRECTORY_SEPARATOR . 'deploy/openhab-command.phar',
+	'openhab-sitemap.php' => '.' . DIRECTORY_SEPARATOR . 'deploy/openhab-sitemap.phar'
+);
 
+foreach ($pharFiles as $indexFile => $compileTarget) {
+	$compiler->addIndexFile($indexFile);
+	$compiler->addFile('vendor/autoload.php');
+	$compiler->addFile('config.php');
+	$compiler->addDirectory('vendor/composer', '!*.php');
+	$compiler->addDirectory('vendor/zendframework', '!*.php');
+	$compiler->addDirectory('vendor/league', '!*.php');
+	$compiler->addDirectory('src', '!*.php');
+	$compiler->compile($compileTarget);
+	chmod($compileTarget, 0755);
 
-$compiler->addIndexFile('openhab-command.php');
-$compiler->addFile('vendor/autoload.php');
-$compiler->addFile('config.php');
-$compiler->addDirectory('vendor/composer', '!*.php');
-$compiler->addDirectory('vendor/zendframework', '!*.php');
-$compiler->addDirectory('vendor/league', '!*.php');
-$compiler->addDirectory('src', '!*.php');
-$compiler->compile('.' . DIRECTORY_SEPARATOR . 'deploy/openhab-command.phar');
-
-
-$compiler->addIndexFile('openhab-sitemap.php');
-$compiler->addFile('vendor/autoload.php');
-$compiler->addFile('config.php');
-$compiler->addDirectory('vendor/composer', '!*.php');
-$compiler->addDirectory('vendor/zendframework', '!*.php');
-$compiler->addDirectory('vendor/league', '!*.php');
-$compiler->addDirectory('src', '!*.php');
-$compiler->compile('.' . DIRECTORY_SEPARATOR . 'deploy/openhab-sitemap.phar');
+	if (true === defined('DEPLOY_TARGET')) {
+		copy($compileTarget, DEPLOY_TARGET . pathinfo($compileTarget)['filename']);
+	}
+}
