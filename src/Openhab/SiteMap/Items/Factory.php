73ce2026f60ed0e35f0ee99e7be6d7ac5792a8c3
ms <schmiddim@gmx.at>
2016-01-31 02:16:35 +0100

    refactoring

--- src/Openhab/SiteMap/Items/Factory.php
+++ src/Openhab/SiteMap/Items/Factory.php
@@ -25,82 +25,68 @@ class Factory
 			$object->setType((string)$element->type);
 			$object->setIcon((string)$element->icon);
 			$object->setWidgetId((string)$element->widgetId);
+			//@todo - refactor this crap
 			if (is_object($element->item)) {
 				$object->addChildren(self::createBySimpleXmlElement($element->widget));
 			}
-			if (is_object($element->linkedPage->widget)) {
-				foreach ($element->linkedPage->widget as $widget) {
-					$object->addChildren(self::createBySimpleXmlElement($widget));
-				}
-			}
+
 
 
 		} elseif ((string)$element->type === 'Group') {
 			$object = new Group();
 			$object->populateBySimpleXmlElement($element);
+			//@todo - refactor this crap
 			if (is_object($element->item)) {
 				$object->addChildren(self::createBySimpleXmlElement($element->item));
 			}
 
-			if (is_object($element->linkedPage->widget)) {
 
-				foreach ($element->linkedPage->widget as $widget) {
-					$object->addChildren(self::createBySimpleXmlElement($widget));
-
-				}
-			}
 			
 		} elseif ((string)$element->type === 'Text') {
 			$object = new TextItem();
 			$object->populateBySimpleXmlElement($element);
+			//@todo - refactor this crap
 			if (is_object($element->item)) {
 				$object->addChildren(self::createBySimpleXmlElement($element->item));
 			}
-			if (is_object($element->linkedPage->widget)) {
-				foreach ($element->linkedPage->widget as $widget) {
-					$object->addChildren(self::createBySimpleXmlElement($widget));
-				}
-			}
+
 			
 		} elseif ((string)$element->type === 'Switch') {
 			$object = new SwitchItem();
 			$object->populateBySimpleXmlElement($element);
+			//@todo - refactor this crap
 			if (is_object($element->item)) {
 				$object->addChildren(self::createBySimpleXmlElement($element->item));
-				
+
 			}
-			$break = true;
+
 		} elseif ((string)$element->type === 'SwitchItem') {
 			$object = new OpenhabSwitchItem();
 			$object->populateBySimpleXmlElement($element);
-			
-
 		} elseif ((string)$element->type === 'DimmerItem') {
 			$object = new OpenhabDimmerItem();
 			$object->populateBySimpleXmlElement($element);
-			
-
 		} elseif ((string)$element->type === 'GroupItem') {
 			$object = new Item();
 			$object->populateBySimpleXmlElement($element);
-			
-
 		} elseif ((string)$element->type === 'DateTimeItem') {
 			$object = new DateTimeItem();
 			$object->populateBySimpleXmlElement($element);
-			
 		} elseif ((string)$element->type === 'NumberItem') {
 			$object = new NumberItem();
 			$object->populateBySimpleXmlElement($element);
-			
 		} elseif ((string)$element->type === 'StringItem') {
 			$object = new StringItem();
 			$object->populateBySimpleXmlElement($element);
-
 		}
 
 
 
+		if (is_object($element->linkedPage->widget)) {
+			foreach ($element->linkedPage->widget as $widget) {
+				$object->addChildren(self::createBySimpleXmlElement($widget));
+			}
+		}
 
 		if ($object !== null) {
 			return $object;
